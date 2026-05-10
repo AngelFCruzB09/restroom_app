cbb089d0b58dc9d3c7103414e9c4b8da31a92076
luchy09 <angelcruzb09@gmail.com>
2026-05-10 14:09:53 -0600

    feat: implement database connection with Supabase and setup .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:restroom_app/login_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['URL']!,
+    anonKey: dotenv.env['ANONKEY']!,
+  );
+  final authStatus = Supabase.instance.client.auth.currentSession;
+  debugPrint('Estado: $authStatus');
   runApp(const MyApp());
 }
 
